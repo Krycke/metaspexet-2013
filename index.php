8ac453b0c2b5faec2b1aebf4f0dfbdaed1be5af6
root <root@nero.cleric.nu>
2013-03-03 01:40:37 +0100

    Links and WordPress stuff

--- index.php
+++ index.php
@@ -1,88 +1,68 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+get_header();
+?>
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+<div id="primary" class="site-content" >
+    <div id="main">
 
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory') ?>/normalize.css">
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory') ?>/style.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
+        <?php if (have_posts()) : ?>
 
-		<div id="train"></div>
-		<div id="mag_glass"></div>
-		
-        <div class="container_12" id="wrapper">
-			
-				<div id="gun"></div>
-			
-		
-            <header id="logo" class="ir prefix_3 grid_6 suffix_3">
-                <hgroup>
-                    <h1>MetaSpexet</h1>
-                    <h2>Mordet på Occidentexpressen - eller en mördare på spåren</h2>
-                </hgroup>
-            </header>
-            <div id="leftSidebar" class="prefix_1 grid_2">
-				<div id="buyTickets" class="grid_2 alpha omega"></div>
-				<div id="key" class="grid_2 alpha omega "></div>
-                <nav id="nav" class="grid_2 alpha omega">
-                    <ul id="nav_list">
-                        <li>Hitta Hit</li>
-                        <li>Om oss</li>
-                        <li>Sponsor</li>
-                        <li>Trailer</li>
-                    </ul>
-                </nav>
-            </div>
-            <div id="main_wrapper" class="grid_6">
-                <div id="main_top"></div>
-                <div id="main"></div>
-                <div id="main_bottom"></div>
-            </div>
-            <div id="rightSidebar" class="grid_3">
-                <div id="block">
-                    <h2>Var?</h2>
-                    <ul><li>Diselverkstan
-                            <ul><li>vid Slussen</li></ul>
-                        </li></ul>
-                    <h2>När?</h2>
-                    <ul><li>Mellan den 7-9
-                            <ul><li>Juni klockan
-                                    <ul><li>13:37</li></ul>
-                                </li></ul>
-                        </li></ul>
+            <?php /* Start the Loop */ ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <?php get_template_part('content', get_post_format()); ?>
+            <?php endwhile; ?>
 
-                    <a id="hitta_hit">Hitta hit</a>
-                </div>
-            </div>
-        </div>
+            <?php twentytwelve_content_nav('nav-below'); ?>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <?php else : ?>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
-            (function(d, t) {
-                var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-                g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
-                s.parentNode.insertBefore(g, s)
-            }(document, 'script'));
-        </script>
-    </body>
-</html>
+            <article id="post-0" class="post no-results not-found">
+
+                <?php
+                if (current_user_can('edit_posts')) :
+                    // Show a different message to a logged-in user who can add posts.
+                    ?>
+                    <header class="entry-header">
+                        <h1 class="entry-title"><?php _e('No posts to display', 'twentytwelve'); ?></h1>
+                    </header>
+
+                    <div class="entry-content">
+                        <p><?php printf(__('Ready to publish your first post? <a href="%s">Get started here</a>.', 'twentytwelve'), admin_url('post-new.php')); ?></p>
+                    </div><!-- .entry-content -->
+
+                    <?php
+                else :
+                    // Show the default message to everyone else.
+                    ?>
+                    <header class="entry-header">
+                        <h1 class="entry-title"><?php _e('Nothing Found', 'twentytwelve'); ?></h1>
+                    </header>
+
+                    <div class="entry-content">
+                        <p><?php _e('Apologies, but no results were found. Perhaps searching will help find a related post.', 'twentytwelve'); ?></p>
+                        <?php get_search_form(); ?>
+                    </div><!-- .entry-content -->
+                <?php endif; // end current_user_can() check  ?>
+
+            </article><!-- #post-0 -->
+
+        <?php endif; // end have_posts() check  ?>
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
