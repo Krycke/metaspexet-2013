be7656254c8c8372a7685c734e31a970b92448ac
root <root@nero.cleric.nu>
2013-04-11 21:15:46 +0200

    Realeased version

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <div id="rightSidebar" class="grid_3">
     <div id="block">
         <h2>Var?</h2>
-        <ul><li><h3>Diselverkstan</h3>
+        <ul><li><h3><a href="http://dieselverkstaden.se/oppettider-hitta-hit/" target="_blank" title="Dieselverkstaden">Dieselverkstaden</a></h3>
                 <ul><li>7 min från slussen</li></ul>
             </li></ul>
         <h2>När?</h2>
@@ -18,20 +18,20 @@
 </div>
 </div>
 </div>
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri() ?>/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
-<script src="<?php echo get_template_directory_uri() ?>/js/plugins.js"></script>
-<script src="<?php echo get_template_directory_uri() ?>/js/main.js"></script>
+<?php wp_footer() ?>
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>-->
+<!--<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri() ?>/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>-->
+<!--<script src="<?php echo get_template_directory_uri() ?>/js/plugins.js"></script>-->
+<!--<script src="<?php echo get_template_directory_uri() ?>/js/main.js"></script>-->
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
+<!--<script>
     var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
     (function(d, t) {
         var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
         g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
         s.parentNode.insertBefore(g, s)
     }(document, 'script'));
-</script>
+</script>-->
 </body>
 </html>
